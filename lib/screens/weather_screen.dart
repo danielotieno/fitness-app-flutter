1ed0691d572189414366ef1efdf05049c920b0d5
Daniel Otieno <danielotieno.ke@gmail.com>
2021-10-31 09:43:17 +0300

    chore:create a session model

--- lib/screens/weather_screen.dart
+++ lib/screens/weather_screen.dart
@@ -34,7 +34,14 @@ class _WeatherScreenState extends State<WeatherScreen> {
                         onPressed: getData,
                       )),
                 ),
-              )
+              ),
+              WeatherRow('Name of the City: ', result.name),
+              WeatherRow('Description: ', result.description),
+              WeatherRow(
+                  'Temperature: ', result.temperature.toStringAsFixed(2)),
+              WeatherRow('Perceived: ', result.perceived.toStringAsFixed(2)),
+              WeatherRow('Pressure: ', result.pressure.toString()),
+              WeatherRow('Humidity: ', result.humidity.toString()),
             ],
           ),
         ));
@@ -44,5 +51,26 @@ class _WeatherScreenState extends State<WeatherScreen> {
     HttpHelper helper = HttpHelper();
     result = await helper.getWeather(textCity.text);
     setState(() {});
+  }
+
+  Widget WeatherRow(String label, String value) {
+    Widget row = Padding(
+      padding: EdgeInsets.symmetric(vertical: 16),
+      child: Row(
+        children: [
+          Expanded(
+              flex: 3,
+              child: Text(label,
+                  style: TextStyle(
+                      fontSize: 20, color: Theme.of(context).hintColor))),
+          Expanded(
+              flex: 4,
+              child: Text(value,
+                  style: TextStyle(
+                      fontSize: 20, color: Theme.of(context).primaryColor))),
+        ],
+      ),
+    );
+    return row;
   }
 }
